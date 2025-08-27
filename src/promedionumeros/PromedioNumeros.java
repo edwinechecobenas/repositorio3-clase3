fa398814e3a77c0a9b0fa3b2e1f788b52aa85b7e
TIP <TIP@PC10X33Feche.certus.edu.pe>
2025-08-26 21:00:34 -0500

    Promedio

---
+++ src/promedionumeros/PromedioNumeros.java
@@ -0,0 +1,25 @@
+
+package promedionumeros;
+
+import java.util.Scanner;
+public class PromedioNumeros {
+
+ 
+    public static void main(String[] args) {
+       double nota1,nota2,promedio;
+       
+       Scanner lectura =new Scanner(System.in);
+       
+       System.out.print("Ingrese Nota 1 : ");
+       nota1 = lectura.nextDouble();       
+       System.out.print("Ingrese Nota 2 : ");
+       nota2= lectura.nextDouble();
+       
+       promedio=(nota1+nota2)/2;
+       System.out.println("Nota 1 : " + nota1);
+       System.out.println("Nota 2 : " + nota2);            
+       System.out.println("El promedio es : " + promedio);
+               
+    }
+    
+}
